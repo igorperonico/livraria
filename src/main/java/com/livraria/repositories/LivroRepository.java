53d44a82dee23be10578564e03e44570b9bca463
igorperonico <igormtpr@gmail.com>
2024-08-07 20:26:56 -0300

    feat: create LivroRepository

---
+++ src/main/java/com/livraria/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.livraria.repositories;
+
+import com.livraria.models.LivroModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LivroRepository extends JpaRepository<LivroModel, UUID> {
+}
