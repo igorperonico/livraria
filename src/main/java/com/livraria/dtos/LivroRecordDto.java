5c5cd42c53ea69ff63c7db71bf01776d44530953
igorperonico <igormtpr@gmail.com>
2024-08-07 21:25:03 -0300

    refactor: include genre field in LivroRecordDto

--- src/main/java/com/livraria/dtos/LivroRecordDto.java
+++ src/main/java/com/livraria/dtos/LivroRecordDto.java
@@ -1,9 +1,11 @@
 package com.livraria.dtos;
 
+import com.livraria.models.GeneroLivro;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record LivroRecordDto(@NotBlank String titulo, @NotNull String autor, @NotNull BigDecimal preco) {
+public record LivroRecordDto(@NotBlank String titulo, @NotBlank String autor, @NotNull BigDecimal preco, @NotNull
+                             GeneroLivro genero) {
 }
