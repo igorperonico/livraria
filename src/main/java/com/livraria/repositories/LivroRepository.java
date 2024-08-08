eaec198798aebee8ce39f8f431e932b72c87244b
igorperonico <igormtpr@gmail.com>
2024-08-07 21:26:20 -0300

    feat: add method to find books by genre

--- src/main/java/com/livraria/repositories/LivroRepository.java
+++ src/main/java/com/livraria/repositories/LivroRepository.java
@@ -1,11 +1,14 @@
 package com.livraria.repositories;
 
+import com.livraria.models.GeneroLivro;
 import com.livraria.models.LivroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface LivroRepository extends JpaRepository<LivroModel, UUID> {
+    List<LivroModel> findByGenero(GeneroLivro genero);
 }
